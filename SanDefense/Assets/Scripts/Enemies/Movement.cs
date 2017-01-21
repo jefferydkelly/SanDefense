a283539c98f2bf8e31af30472ab67f2cefef6c07
Ethan Nichols <ethan.thomas.nichols@gmail.com>
2017-01-20 20:50:05 -0500

    Added Enemy Prefab with movement towards paths. Added path prefab for the path. Added a script that moves the enemy towards the points in increasing order. Made my own scene for testing

---
+++ SanDefense/Assets/Scripts/Enemies/Movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    //The speed of the enemy
+    public float speed;
+
+    //The points for the path the enemy must follow
+    //The point number that the enemy is currently on
+    private GameObject[] points;
+    private int pointNumber = 0;
+
+	// Use this for initialization
+	void Start () {
+        points = GameObject.FindGameObjectsWithTag("Path");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Move the enemy towards the next pathpoint
+        gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, points[pointNumber].transform.position, Time.deltaTime * speed);
+
+        //Test if the enemy is in the same location as the path point
+        if (gameObject.transform.position == points[pointNumber].transform.position)
+        {
+            //Increase the point the enemy should be at
+            pointNumber++;
+
+            //!!!!!TMEPERARY!!!!!//
+            //Test if the enemy has reached the last point
+            //Go back to the first point the enemy started at
+            if (pointNumber >= points.Length)
+            {
+                pointNumber = 0;
+            }
+        }
+    }
+}
