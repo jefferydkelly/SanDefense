111e802d0e9642811f91c66c12a6bfc4df9b4448
Ethan Nichols <ethan.thomas.nichols@gmail.com>
2017-01-21 12:57:43 -0500

    Created a rough layout of the interface while you play the game. Starting to work on scripts for the interface.

---
+++ SanDefense/Assets/Scripts/Layout/GameInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInfo : MonoBehaviour {
+
+    public int creatureWave;
+    public int currentMoney;
+    public int castleHealth;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
